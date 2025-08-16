26804cff4c0d88fc07850e99b0d81566114c7f17
BestRanger28 <BestRanger28@gmail.com>
2025-08-16 19:28:32 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -7,8 +7,9 @@ def home():
     return "Web server is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
+
     t.start()
